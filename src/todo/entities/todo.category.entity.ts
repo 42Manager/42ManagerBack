a98d049914e68e2b751c0ffcf5edf9569fa90292
이성호 <dltjdgh0303@naver.com>
2023-02-10 13:31:39 +0900

    fix: typeorm 쿼리 에러 수정
    * is_share
      * default 값을 true로 설정
    
    * nullable
      * nullable을 true로 설정

--- src/todo/entities/todo.category.entity.ts
+++ src/todo/entities/todo.category.entity.ts
@@ -23,12 +23,12 @@ export class Category {
   @Column({ name: 'name' })
   name: string;
 
-  @Column({ name: 'is_share' })
+  @Column({ name: 'is_share', default: true })
   is_share: boolean;
 
   @CreateDateColumn({ name: 'created_at' })
   created_at: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
+  @UpdateDateColumn({ name: 'updated_at', nullable: true })
   updated_at: Date;
 }

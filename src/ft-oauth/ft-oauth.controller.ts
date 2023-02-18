38c7a9bdae39830c0ecb0c5c9121d63eda30b33e
이성호 <dltjdgh0303@naver.com>
2023-02-18 19:07:15 +0900

    refactor: 명세서 내용 추가
    * description 추가
    * 재발급 API 진입점을 Patch의 token으로 변경

--- src/ft-oauth/ft-oauth.controller.ts
+++ src/ft-oauth/ft-oauth.controller.ts
@@ -3,6 +3,7 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  Patch,
   Post,
   Query,
   Res,
@@ -14,6 +15,7 @@ import {
   ApiInternalServerErrorResponse,
   ApiOkResponse,
   ApiOperation,
+  ApiResponse,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
@@ -27,7 +29,12 @@ export class FtOauthController {
     private readonly config: ConfigService,
   ) {}
 
-  @ApiOperation({ summary: '42 code 발급 (리다이렉션)' })
+  @ApiOperation({
+    summary: '42 code 발급',
+    description:
+      '42 인트라 로그인 창으로 리다이렉션<br>로그인 성공 시 발급된 code를 가지고 /42oauth/token uri의 API로 리다이렉션',
+  })
+  @ApiResponse({ status: 302, description: '리다이렉션 성공' })
   @ApiInternalServerErrorResponse({ description: 'code 발급 실패' })
   @Get('authorize')
   async issue42code(@Res({ passthrough: true }) res: Response) {
@@ -40,13 +47,17 @@ export class FtOauthController {
         )}&response_type=${this.config.get('FT_RESPONSE_TYPE')}`,
       );
     } catch (err) {
-      return new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  @ApiOperation({ summary: '42 access token 발급' })
-  @ApiOkResponse({ description: '42 access token 발급 성공' })
-  @ApiUnauthorizedResponse({ description: '42 access token 발급 실패' })
+  @ApiOperation({
+    summary: '42 token 발급',
+    description:
+      '인트라 로그인 창에서 자동으로 리다이렉션 되어 호출되는 API<br>access token은 body, refresh token은 cookie에 발급',
+  })
+  @ApiOkResponse({ description: '42 token 발급 성공' })
+  @ApiUnauthorizedResponse({ description: '42 token 발급 실패' })
   @Post('token')
   async issue42token(
     @Query('code') code: string,
@@ -66,10 +77,14 @@ export class FtOauthController {
     };
   }
 
-  @ApiOperation({ summary: '42 access token 재발급' })
+  @ApiOperation({
+    summary: '42 token 재발급',
+    description:
+      'cookie에 있는 42 refresh token으로 재발급<br>access token은 body, refresh token은 cookie에 발급',
+  })
   @ApiOkResponse({ description: '42 access token 재발급 성공' })
   @ApiUnauthorizedResponse({ description: '42 access token 재발급 실패' })
-  @Post('token/refresh-token')
+  @Patch('token')
   async reissue42token(
     @Cookies('ftRefreshToken') refreshToken: string,
     @Res({ passthrough: true }) res: Response,

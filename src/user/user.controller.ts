1256c2dea0571d1bdd2119979b0222705338b507
이성호 <dltjdgh0303@naver.com>
2023-02-23 15:31:36 +0900

    feat: user API 명세 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,14 @@ import { UserService } from './user.service';
 import { JwtAccessTokenGuard } from 'src/auth/guard/jwt.auth.guard';
 import { JwtPayload } from 'src/decorator/jwt-payload.decorator';
 import { Account } from 'src/auth/entities/account.entity';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @ApiTags('User')
 @Controller('user')
@@ -11,7 +18,44 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @ApiOperation({ summary: '42 사용자 정보' })
+  @ApiOperation({
+    summary: '42 사용자 정보',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              ftAccessToken: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiOkResponse({
+    description: '성공',
+    schema: {
+      type: 'object',
+      properties: {
+        data: {
+          type: 'object',
+          properties: {
+            blackholedAt: {
+              type: 'string',
+              format: 'date-time',
+              example: 'yyyy-MM-dd HH:mm:ss',
+            },
+            intraId: { type: 'string' },
+            imageUrl: { type: 'string' },
+          },
+        },
+      },
+    },
+  })
+  @ApiBadRequestResponse({ description: '42 카뎃이 아닌 사용자' })
+  @ApiUnauthorizedResponse({ description: '토큰 만료' })
+  @ApiInternalServerErrorResponse({ description: '서버 에러 발생' })
   @Get('42seoul')
   getMyPage42Info(
     @JwtPayload() account: Account,
